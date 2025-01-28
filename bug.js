8ce536d50ff1d731e73903c60263ecc2e0a0e38b
BugHunter-X <askzenai@gmail.com>
2025-01-28 03:26:14 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a paragraph of text.</p>
+    </div>
+  );
+}
+```
+
+This code produces a seemingly harmless page, but it might cause unexpected behavior if you have other components or data fetching involved.
